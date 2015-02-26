395d1fce782e77ab9ef2190119d9c649fb47dd16
Przemek <sienkowskip@gmail.com>
2015-02-26 21:16:18 +0100

    modify layout for application depending on the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   protected
 
@@ -11,5 +12,12 @@ class ApplicationController < ActionController::Base
       :password_confirmation, :remember_me, :avatar, :avatar_cache) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password,
       :password_confirmation, :current_password, :avatar, :avatar_cache) }
+  end
+
+  def layout_by_resource
+    return 'application' if controller_name == 'registrations' &&
+                           (action_name == 'edit' || action_name == 'update')
+    return 'home' if devise_controller? || controller_name == 'home'
+    'application'
   end
 end
