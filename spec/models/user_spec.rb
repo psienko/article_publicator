2f647df7bc4a1f572b001765722397b6296d342c
Przemek <sienkowskip@gmail.com>
2015-03-01 10:26:59 +0100

    modified spec for the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 describe User do
+  let(:user_with_avatar) { create :user }
+  let(:user_with_out_avatar) { create :use, avatar: nil }
+
   it { should validate_presence_of :firstname }
   it { should validate_presence_of :lastname }
+
+  describe '#avatar_path' do
+    it 'returns avatar url when avatar is set' do
+      result = user_with_avatar.avatar_path
+      expect(result).to eq(user_with_avatar.avatar.url)
+    end
+
+    it 'returns default avatar url when avatar is not set' do
+      result = user_with_avatar.avatar_path
+      expect(result).to eq('/uploads/user/avatar/default_avatar.gif')
+    end
+  end
 end
