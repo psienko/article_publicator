9032fdd6be186ff86288d6bfb5a8baafde016826
Przemek <sienkowskip@gmail.com>
2015-03-02 08:56:58 +0100

    add specs

--- spec/features/article_features_spec.rb
+++ spec/features/article_features_spec.rb
@@ -15,13 +15,13 @@ feature 'User can see the article' do
   scenario 'The user who is not the author can not see the unpublished article' do
     log_in_with(user.email, user.password)
     visit article_path(unpublished_article)
-    expect_unpublished_alert
+    expect_error_alert_with(:unpublished)
   end
 
   scenario 'The guest can not see the unpublished article' do
     visit article_path(unpublished_article)
     expect_redirect_to_sign_in_form
-    expect_unpublished_alert
+    expect_error_alert_with(:unpublished)
   end
 
   scenario 'The guest can see the published article' do
@@ -40,54 +40,100 @@ feature 'User can see the article' do
     expect(page).to have_content('MyTitle')
     expect(page).to have_content('MyContent')
   end
-
-  def expect_unpublished_alert
-    expect_error_alert
-    expect(page).to have_content('Access denied! This article has not been published.')
-  end
 end
 
-feature 'Signed in user can create the article' do
+feature 'Signed in user can create and publish the article' do
   let(:user) { create :user }
-  let(:valid_params) { { title: 'MyTitle', content: 'MyContent', published: false, user: user } }
-  let(:invalid_params) { { title: '', content: 'MyContent', published: false, user: user } }
+  let(:valid_params) { { title: 'MyTitle', content: 'MyContent', published: false } }
+  let(:valid_params_with_published) do
+    {
+      title: 'MyTitle',
+      content: 'MyContent',
+      published: true
+    }
+  end
+  let(:invalid_params) { { title: '', content: 'MyContent', published: false } }
 
   scenario 'The guest is redirect to sign in page' do
     visit new_article_path
     expect_redirect_to_sign_in_form
-    expect_lack_of_authentication
+    expect_error_alert_with(:lack_of_authentication)
   end
 
-  scenario 'Signed in user can create article with valid_params' do
+  scenario 'Signed in user can create article with valid params' do
+    log_in_with(user.email, user.password)
     visit new_article_path
     fill_form(valid_params)
-    expect_success
+    expect_success(:created)
   end
 
-  def expect_success
-    expect_success_alert
-    expect(page).to have_content('The article has been successfully created.')
+  scenario 'Signed in user can publish article with valid params' do
+    log_in_with(user.email, user.password)
+    visit new_article_path
+    fill_form(valid_params_with_published)
+    expect_success(:published)
+  end
+
+  scenario 'Signed in user can not save or publish article with invalid params' do
+    log_in_with(user.email, user.password)
+    visit new_article_path
+    fill_form(invalid_params)
+    expect_error_alert_with(:errors)
+  end
+end
+
+feature 'The author can update and publish his article' do
+  let(:author) { create :user }
+  let(:user) { create :user }
+  let(:article) { create :article, user: author }
+  let(:valid_params) { { title: 'MyTitle', content: 'MyContent', published: false } }
+  let(:valid_params_with_published) do
+    {
+      title: 'MyTitle',
+      content: 'MyContent',
+      published: true
+    }
+  end
+  let(:invalid_params) { { title: '', content: 'MyContent', published: false } }
+
+  scenario 'The guest is redirect to sign in page' do
+    visit edit_article_path(article)
+    expect_redirect_to_sign_in_form
+    expect_error_alert_with(:lack_of_authentication)
+  end
+
+  scenario 'The user who is not the author can not update article' do
+    log_in_with(user.email, user.password)
+    visit edit_article_path(article)
+    fill_form(valid_params)
+    expect_error_alert_with(:access_denied)
+  end
+
+  scenario 'The author can update article with valid params' do
+    log_in_with(author.email, author.password)
+    visit edit_article_path(article)
+    fill_form(valid_params)
+    expect_success(:updated)
+  end
+
+  scenario 'The author can publish article with valid params' do
+    log_in_with(author.email, author.password)
+    visit edit_article_path(article)
+    fill_form(valid_params_with_published)
+    expect_success(:published)
   end
 end
 
 def fill_form(params)
   fill_in 'Title', with: params[:title]
   fill_in 'Content',  with: params[:content]
-  click_button 'Save'
+  click_button 'Save' unless params[:published]
+  click_button 'Publish' if params[:published]
 end
 
-def expect_access_denied
-  expect_error_alert
-  expect(page).to have_content('Access denied!')
-end
 
 def expect_redirect_to_sign_in_form
   expect(current_path).to eq(new_user_session_path)
-end
-
-def expect_lack_of_authentication
-  expect_error_alert
-  expect(page).to have_content('You need to sign in or sign up before continuing.')
 end
 
 def expect_error_alert
@@ -96,4 +142,30 @@ end
 
 def expect_success_alert
   expect(page).to have_css('div.alert.alert-success')
+end
+
+def expect_error_alert_with(type_of_alert)
+  expect_error_alert
+  case type_of_alert
+  when :unpublished
+    expect(page).to have_content('Access denied! This article has not been published.')
+  when :access_denied
+    expect(page).to have_content('Access denied!')
+  when :lack_of_authentication
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  when :errors
+    expect(page).to have_content('Please correct the errors below.')
+  end
+end
+
+def expect_success(type)
+  expect_success_alert
+  case type
+  when :created
+    expect(page).to have_content('The article has been successfully created.')
+  when :published
+    expect(page).to have_content('Your article has been published.')
+   when :updated
+    expect(page).to have_content('Your article has been updated.')
+  end
 end
