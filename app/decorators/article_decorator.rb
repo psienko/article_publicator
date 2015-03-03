0ea58ed417453d39744af5f5af4bc0340a47e912
Przemek <sienkowskip@gmail.com>
2015-03-03 16:46:09 +0100

    added methods in ArticleDecorator

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,13 +1,27 @@
 class ArticleDecorator < Draper::Decorator
   delegate_all
+  include Draper::LazyHelpers
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def read_button(size_class = nil)
+      link_to 'Read', article_path(object), class: "btn btn-primary #{size_class}"
+  end
 
+  def edit_button(size_class = nil)
+    return nil if object.user != current_user
+    link_to 'Edit', edit_article_path(object), class: "btn btn-success #{size_class}"
+  end
+
+  def delete_button(size_class = nil)
+    return nil if object.user != current_user
+    link_to 'Delete', article_path(article), class: "btn btn-danger #{size_class}",
+      method: :delete, data: { confirm: 'Are you sure?' }
+  end
+
+  def author
+    "#{object.user.firstname.capitalize} #{object.user.lastname.capitalize}" if object.user
+  end
+
+  def short_content
+    truncate(object.content, length: 200)
+  end
 end
