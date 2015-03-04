f38cedd2084d56d30bfd678188d53d315b1ad624
Przemek <sienkowskip@gmail.com>
2015-03-04 16:36:52 +0100

    modified decorator

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -3,18 +3,27 @@ class ArticleDecorator < Draper::Decorator
   include Draper::LazyHelpers
 
   def read_button(size_class = nil)
-      link_to 'Read', article_path(object), class: "btn btn-primary #{size_class}"
+      link_to article_path(object), class: "btn btn-primary #{size_class}" do
+        concat 'Read ' 
+        concat content_tag :span, '', class: 'glyphicon glyphicon-eye-open'
+      end
   end
 
   def edit_button(size_class = nil)
     return nil if object.user != current_user
-    link_to 'Edit', edit_article_path(object), class: "btn btn-success #{size_class}"
+    link_to edit_article_path(object), class: "btn btn-success #{size_class}" do
+      concat 'Edit ' 
+      concat content_tag :span, '', class: 'glyphicon glyphicon-edit'
+    end
   end
 
   def delete_button(size_class = nil)
     return nil if object.user != current_user
-    link_to 'Delete', article_path(article), class: "btn btn-danger #{size_class}",
-      method: :delete, data: { confirm: 'Are you sure?' }
+    link_to article_path(article), class: "btn btn-danger #{size_class}",
+      method: :delete, data: { confirm: 'Are you sure?' } do
+      concat 'Delete ' 
+      concat content_tag :span, '', class: 'glyphicon glyphicon-trash'
+    end  
   end
 
   def author
