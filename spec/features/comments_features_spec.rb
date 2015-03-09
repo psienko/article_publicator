88f24cf69bbcc3fffe24ec69fd7959ec70f2ee7e
Przemek <sienkowskip@gmail.com>
2015-03-09 21:42:07 +0100

    modified spec

--- spec/features/comments_features_spec.rb
+++ spec/features/comments_features_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-feature 'All users can see the list of comments' do
+feature 'All users can see the list of comments', :js do
   let(:user) { create :user, firstname: 'John', lastname: 'Doe' }
-  let(:article) { create :article }
+  let(:article) { create :article, published: true }
   let(:date) {  DateTime.new(2015, 1, 1, 0, 0, 0, '+0') }
-  # .strftime'%d-%m-%Y at %H:%M'
 
   before do
-    @comment = create :comment, user: user
+    @comment = create :comment, commentable: article, user: user, created_at: date
   end
   
   scenario 'A visitor visit the article page and see all comments' do
@@ -17,14 +16,14 @@ feature 'All users can see the list of comments' do
 
   def expect_comment(comment)
     expect(page).to have_css(".comment-#{comment.id}", text: 'MyComment')
-    expect(page).to have_css(".comment-#{comment.id}.answered-by", text: 'John Doe')
-    expect(page).to have_css(".comment-#{comment.id}.answered-at", text: '01-01-2015 at 00:00')
+    expect(page).to have_css(".comment-#{comment.id}.posted-by", text: 'John Doe')
+    expect(page).to have_css(".comment-#{comment.id}.posted-at", text: '01-01-2015 at 00:00')
   end
 end
 
-feature 'Signed in users can add a comment' do
+feature 'Signed in users can add a comment', :js do
   let(:user) { create :user, firstname: 'John', lastname: 'Doe' }
-  let(:article) { create :article }
+  let(:article) { create :article, published: true }
   let(:date) {  DateTime.new(2015, 1, 1, 0, 0, 0, '+0') }
   
   scenario 'Signed in user can see added comment' do
@@ -36,8 +35,7 @@ feature 'Signed in users can add a comment' do
 
   scenario 'A guest can not add comment and is redirect to login page' do
     visit article_path(article)
-    add_comment('MyNewComment')
-    expect_redirect_to_sign_in_form
+    expect(page).to have_content('Sign in to add comment.')
   end
 
   def expect_redirect_to_sign_in_form
@@ -46,12 +44,15 @@ feature 'Signed in users can add a comment' do
   end
 
   def add_comment(body)
-    fill_in 'New comment', with: body
-    click_button 'Comment'
+    within("#comment_form") do
+      fill_in 'new_comment', with: body
+      click_button 'Comment'
+    end
+    binding.pry
   end
 
   def expect_comment
     new_comment = Comment.find_by_body('MyNewComment')
-    expect(page).to have_css(".comment-#{new_comment.id}", text: 'MyNewComment')
+    expect(page).to have_css(".comment", text: 'MyNewComment')
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,12 +3,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/poltergeist'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
+Capybara.default_driver = :poltergeist
 
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
@@ -22,6 +25,9 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  
+  Capybara.javascript_driver = :poltergeist
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, phantomjs: Phantomjs.path)
+  end
   config.infer_spec_type_from_file_location!
 end
