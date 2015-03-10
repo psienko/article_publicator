7bbe05fcb0270375d94be9d17fa283f2335063f7
Przemek <sienkowskip@gmail.com>
2015-03-10 10:07:33 +0100

    fix bug with incorrect path to resource

--- app/assets/javascripts/angular/services/comments_service.js
+++ app/assets/javascripts/angular/services/comments_service.js
@@ -1,3 +1,3 @@
 app.factory('Comment', ['railsResourceFactory', function (railsResourceFactory) {
-  return railsResourceFactory({url: '4/comments/{{id}}', name: 'comment'});
+  return railsResourceFactory({url: '/articles/'+window.Id+'/comments', name: 'comment'});
 }]);
